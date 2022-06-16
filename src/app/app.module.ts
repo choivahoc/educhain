297abd0c66f45461f81b40b4119089c50544973c
dinhtuan99 <vdt12319999@gmail.com>
2022-06-16 23:37:54 +0700

    Add user service, setting form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,8 @@ import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pa
 import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -85,7 +87,12 @@ import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user
     LandingComponent,
     ProfileComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/cards/card-settings/card-settings.component.ts
+++ src/app/components/cards/card-settings/card-settings.component.ts
@@ -1,11 +1,48 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "app-card-settings",
   templateUrl: "./card-settings.component.html",
 })
 export class CardSettingsComponent implements OnInit {
-  constructor() {}
+  settingsForm!: FormGroup;
+  isSubmit: boolean = false;
+  constructor(private fb: FormBuilder, private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getUsersByType("student").subscribe((res) => {
+      console.log(res);
+    });
+
+    this.settingsForm = this.fb.group({
+      imageUrl: this.fb.control("", [Validators.required]),
+      fullName: this.fb.control("", [Validators.required]),
+      gender: this.fb.control("", [Validators.required]),
+      email: this.fb.control("", [Validators.required, Validators.email]),
+      address: this.fb.control("", [Validators.required]),
+      dateOfBirth: this.fb.control("", [Validators.required]),
+      city: this.fb.control("", [Validators.required]),
+      phone: this.fb.control("", [Validators.required]),
+      identityCard: this.fb.group({
+        no: this.fb.control("", [Validators.required]),
+        dateOfIssue: this.fb.control("", [Validators.required]),
+        dateOfExprity: this.fb.control("", [Validators.required]),
+      }),
+    });
+    // this.userService.getCurrentUser().subscribe(data => {
+    //   this.currentUser = data;
+    //   this.settingsForm.patchValue({
+    //     imageUrl: this.currentUser.user.image,
+    //     username: this.currentUser.user.username,
+    //     bio: this.currentUser.user.bio,
+    //     email: this.currentUser.user.email
+    //   })
+    // })
+  }
+
+  formSubmit() {
+    console.log(this.settingsForm.value);
+  }
 }
